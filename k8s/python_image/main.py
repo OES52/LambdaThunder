b44ae4b2693d0961899aef6319edfe9eda7819f3
OES52 <choehanbeen@gmail.com>
2023-12-03 15:10:08 +0900

    update main.py

--- k8s/python_image/main.py
+++ k8s/python_image/main.py
@@ -1,3 +1,30 @@
-from test import main
-
-print(main())
+from test import main                           
+import io                                       
+import sys                                      
+import json                                     
+import os                                       
+                                                
+arg = os.environ.get('ARGS')                    
+answer = os.environ.get('ANSWER')               
+json_arg = json.loads(arg.replace("'","\""))    
+                                                
+captured_output = io.StringIO()                 
+sys.stdout = captured_output                    
+                                                
+return_output = main(*json_arg)                 
+                                                
+sys.stdout = sys.__stdout__                     
+print_output = captured_output.getvalue()       
+                                                
+correct = str(return_output)==answer            
+                                                
+data = {                                        
+    "Print" : print_output,                     
+    "Return" : str(return_output),              
+    "Solution" : answer,                        
+    "Win" : correct                             
+}                                               
+                                                
+json_string = json.dumps(data)                  
+                                                
+print(json_string)                              
